24420cee2265c33cf4b709ed0f668ce36c03a034
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2012-03-13 16:11:36 +0800

    Try Depoly

---
+++ config/deploy/production.rb
@@ -0,0 +1,36 @@
+require 'bundler/capistrano'
+# require 'hoptoad_notifier/capistrano'
+
+set :application, "jackmodo_app"
+
+set :branch, "master"
+set :repository,  "git://github.com/HumbleLivelyBear2/index_beta_app.git"
+set :scm, "git"
+set :user, "apps" # 一個伺服器上的帳戶用來放你的應用程式，不需要有sudo權限，但是需要有權限可以讀取Git repository拿到原始碼
+set :port, "22"
+
+set :deploy_to, "/home/apps"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+role :web, "106.187.42.93"
+role :app, "106.187.42.93"
+role :db,  "106.187.42.93", :primary => true
+
+namespace :deploy do
+
+  task :copy_config_files, :roles => [:app] do
+    db_config = "#{shared_path}/database.yml"
+    run "cp #{db_config} #{release_path}/config/database.yml"
+  end
+  
+  task :update_symlink do
+    run "ln -s {shared_path}/public/system {current_path}/public/system"
+  end
+  
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
